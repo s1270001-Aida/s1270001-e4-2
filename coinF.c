78438ecf4ecfaf48010b0b9c70127a98f2c8b751
s1270001-gorokichi <49747881+s1270001-Aida@users.noreply.github.com>
2020-08-06 20:29:27 +0900

    Heads or Tails

---
+++ coinF.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+  int i,b=0,heads=0,tails=0;
+  char a[10];
+
+  printf("Tossing a coin...\n");
+  srand(time(NULL));
+  for(i=1;i<4;i++)
+    {
+      b=rand()%2+1;
+      if(b==1)
+	{
+	  printf("round%d:Heads\n",i);
+	  heads++;	  
+	}
+      else if(b==2)
+	{
+	  printf("round%d:Tails\n",i);
+	  tails++;
+	}
+     
+    }
+  printf("Heads:%d, Tails:%d\n",heads,tails);
+
+  return 0;
+}
